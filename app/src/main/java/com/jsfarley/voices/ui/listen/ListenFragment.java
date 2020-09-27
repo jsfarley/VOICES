257085a3006eec5934f601b27da50ef9cdbcce1c
Sean Farley <james.s.farley@gmail.om>
2020-09-27 16:02:47 -0500

    lifecycle view updated

--- app/src/main/java/com/jsfarley/voices/ui/listen/ListenFragment.java
+++ app/src/main/java/com/jsfarley/voices/ui/listen/ListenFragment.java
@@ -12,6 +12,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -46,7 +47,8 @@ public class ListenFragment extends Fragment {
 
 	public View onCreateView(@NonNull LayoutInflater inflater,
 	                         ViewGroup container, Bundle savedInstanceState) {
-		mListenViewModel = ViewModelProviders.of(this).get(ListenViewModel.class);
+		mListenViewModel = new ViewModelProvider(this).get(ListenViewModel.class);
+		//mListenViewModel = ViewModelProviders.of(this).get(ListenViewModel.class);
 		View root = inflater.inflate(R.layout.fragment_listen, container, false);
 		trackTitle = new ArrayList<>();
 		trackIcon = new ArrayList<>();
